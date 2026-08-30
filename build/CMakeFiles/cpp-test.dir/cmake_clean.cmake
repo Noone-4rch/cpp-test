8244fe2a7ffd2a0df46b8f0463e42c8f0a347817
Noone <no9609987@gmail.com>
2026-08-30 22:48:59 +0200

    Initial commit

---
+++ build/CMakeFiles/cpp-test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-test.dir/link.d"
+  "CMakeFiles/cpp-test.dir/src/main.cpp.o"
+  "CMakeFiles/cpp-test.dir/src/main.cpp.o.d"
+  "cpp-test"
+  "cpp-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
